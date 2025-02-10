573eca19b41e9696a2d015748bddbd8f9e68de99
Neal Guarddin <neal@Neals-MacBook-Air.local>
2025-02-10 10:04:13 +0700

    First push HelloCSUI.java

---
+++ HelloCSUI.java
@@ -0,0 +1,19 @@
+// This is a single-line comment
+
+/*
+$ git remote add origin https://github.com/OWNER/REPOSITORY.git
+# Set a new remote
+
+1. Set remote dulu. List remote dengan "git remote -v", untuk munculin list remote github
+2. "git remote set-url origin <url-baru>" untuk pindah remote
+3. "git add ." untuk add semua file yang ada di directory local kita
+4. "git commit -m <message>" untuk ngasih tahu perubahan di dalam github
+5. "git push -u origin main" untuk push/masukin perubahan baru ke github link
+ */
+
+public class HelloCSUI {
+    public static void main(String[] args) {
+        System.out.println(2*5);
+        System.out.println("Selamat Datang di DDP 2!");
+    }
+}
