13477e870932d8ea1ceeb75c7a27af795a078ee1
Neal Guarddin <neal@Neals-MacBook-Air.local>
2025-02-10 10:29:08 +0700

    Tambah ForLoops.java

---
+++ ForLoops.java
@@ -0,0 +1,10 @@
+public class ForLoops {
+    public static void main(String[] args) {
+
+        // Output "Welcome to Java! sampai 100x"
+        int i;
+        for (i = 0; i < 100; i++) {
+            System.out.println("Welcome to Java!");
+        }
+    }
+}
